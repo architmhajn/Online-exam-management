ee7736eae1aac8336afe9ffa4a011fb8bd2a83d0
BHAVYA MAHAJAN <mahajanb26@gmail.com>
2026-01-20 22:47:57 +0530

    changing my mistakes

--- backend/middleware/antiCheating.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.body.cheatingFlags?.includes('tabSwitch') || req.body.cheatingFlags?.includes('fullscreenExit')) {
-    req.cheatingDetected = true;
-  }
-  next();
-};
