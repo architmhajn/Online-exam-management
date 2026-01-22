5f5210a5b4e82d76e704291b7b4b6ce9b175bc55
Archit Mahajan <mahajanarchit83@gmail.com>
2026-01-23 00:46:28 +0530

    Some major SQL changes done

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
+const adminRoutes = require("./routes/admin");
+app.use("/api/admin", adminRoutes);
 
 
 app.get("/", (req, res) => {
